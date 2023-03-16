7bcfecaf500b3df933a2e299f5b467b95f3347fa
Azerals <3231202800@qq.com>
2023-03-16 19:17:04 +0800

    linux cat implement

---
+++ src/mycat.c
@@ -0,0 +1,36 @@
+#include <dirent.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void cat(const char* path);
+
+void cat(const char* path) {
+    struct stat _s;
+    if (stat(path, &_s) == -1) {
+        printf("cat: %s: No such file or directory\n", path);
+        return;
+    }
+    if (S_ISDIR(_s.st_mode)) {
+        printf("cat: %s: Is a directory\n", path);
+        return;
+    }
+    int code = open(path, O_RDONLY);
+    char buffer[BUFSIZ];
+    int len = 0;
+    while ((len = read(code, buffer, BUFSIZ)) > 0)
+    {
+        write(STDOUT_FILENO, buffer, len);
+    }
+    putchar('\n');
+}
+
+int main(int argc, char *argv[]) {
+    for (int i = 1; i < argc; ++i) {
+        cat(argv[i]);
+    }
+}
