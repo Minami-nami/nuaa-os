3a8553536970e19613ec1aed3a0c15b484384b82
Azerals <3231202800@qq.com>
2023-03-19 08:44:20 +0800

    linux cat implement

--- src/mycat.c
+++ src/mycat.c
@@ -3,34 +3,38 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
-void cat(const char* path);
+int  ret = EXIT_SUCCESS;
+void cat(const char *path);
 
-void cat(const char* path) {
+void cat(const char *path) {
     struct stat _s;
-    if (stat(path, &_s) == -1) {
+    if(stat(path, &_s) == -1) {
         printf("cat: %s: No such file or directory\n", path);
+        ret = EXIT_FAILURE;
         return;
     }
-    if (S_ISDIR(_s.st_mode)) {
+    if(S_ISDIR(_s.st_mode)) {
         printf("cat: %s: Is a directory\n", path);
+        ret = EXIT_FAILURE;
         return;
     }
-    int code = open(path, O_RDONLY);
+    int  code = open(path, O_RDONLY);
     char buffer[BUFSIZ];
-    int len = 0;
-    while ((len = read(code, buffer, BUFSIZ)) > 0)
-    {
+    int  len = 0;
+    while((len = read(code, buffer, BUFSIZ)) > 0) {
         write(STDOUT_FILENO, buffer, len);
     }
     putchar('\n');
 }
 
 int main(int argc, char *argv[]) {
-    for (int i = 1; i < argc; ++i) {
+    for(int i = 1; i < argc; ++i) {
         cat(argv[i]);
     }
+    return ret;
 }
